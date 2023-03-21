e3a8fd7f659913d9acd3757b05d5217f0886d9a5
jonz132 <nietes.jonzeecarel@gmail.com>
2023-03-21 15:01:04 +0800

    Need a remove button for the ticket form

--- javascript.js
+++ javascript.js
@@ -4,4 +4,13 @@ const navMenu = document.querySelector(".navMenu")
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+})
+
+
+const ticketlist = document.querySelector(".openticketlist")
+const form = document.querySelector(".ticketform")
+
+ticketlist.addEventListener("click", () => {
+    ticketlist.classList.toggle("active");
+    form.classList.toggle("active");
 }) 
