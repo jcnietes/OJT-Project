5ac7088320dd8bdc2f2131249193aa1832eea4c0
jonz132 <nietes.jonzeecarel@gmail.com>
2023-03-22 15:01:39 +0800

    Calculator

--- javascript.js
+++ javascript.js
@@ -1,16 +1,33 @@
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".navMenu")
 
+const ticketlist = document.querySelector(".openticketlist")
+const form = document.querySelector(".ticketform")
+
+const hamburger1 = document.querySelector(".hamburger1")
+const form2 = document.querySelector(".ticketform")
+
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
 
-
-const ticketlist = document.querySelector(".openticketlist")
-const form = document.querySelector(".ticketform")
-
 ticketlist.addEventListener("click", () => {
     ticketlist.classList.toggle("active");
     form.classList.toggle("active");
-}) 
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+})
+
+hamburger1.addEventListener("click", () => {
+    ticketlist.classList.remove("active");
+    form2.classList.remove("active");
+    navMenu.classList.remove("active");
+})
+
+
+function LoginButton() {
+    window.location.href = "ReplicatedPage.html";
+}
+
